c59f76d793b1a37d376f285427eb2275fa60748d
CRClothier <c.r.clothier@gmail.com>
2022-05-16 15:17:15 -0500

    Make of_age? private

--- person.rb
+++ person.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(age, name = 'unknown', parent_permission = true)
+  def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -7,14 +7,14 @@ class Person
   end
 
   attr_reader :id
-  attr_accessor :name
-  attr_accessor :age
-
-  def of_age?
-    @age >= 18
+  attr_accessor :name, :age
+  
+  def can_use_services?
+    of_age? || @parent_permission == true
   end
 
-  def can_use_services?
-    is_of_age? || @parent_permission == true
+  private
+  def of_age?
+    @age >= 18
   end
 end
