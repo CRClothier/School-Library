b12376a47aab7b5281274401e8fc54e22599abe5
CRClothier <c.r.clothier@gmail.com>
2022-05-16 12:54:14 -0500

    Complete person class

--- person.rb
+++ person.rb
@@ -1,6 +1,20 @@
-
 class Person
-  def initialize
-    
+  def initialize(name = "unknown", parent_permission = true, age)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_reader :id
+  attr_accessor :name
+  attr_accessor :age
+
+  def is_of_age?
+    @age >= 18
+  end
+
+  def can_use_services?
+    is_of_age? || @parent_permission == true
   end
 end
