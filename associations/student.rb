25dd0f5b2c5ea288481101ad19f15f7dc885038c
CRClothier <c.r.clothier@gmail.com>
2022-05-17 14:39:43 -0500

    Add classroom-student relationships

--- associations/student.rb
+++ associations/student.rb
@@ -1,9 +1,18 @@
 require './person'
+require './classroom'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, classroom, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+    classroom.students << self
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless owner.animals.include?(self)
   end
 
   def play_hooky
