6682a593739575531d99ab4ae0fb0c6370dcb587
CRClothier <c.r.clothier@gmail.com>
2022-05-17 01:14:52 -0500

    remove whitespace

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ require './nameable'
 class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
-  
+
   def initialize(age, name = 'unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
