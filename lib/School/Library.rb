38d62c50f64aeb922b53c074838e6ea9744fc809
CRClothier <c.r.clothier@gmail.com>
2022-05-25 14:43:08 -0500

    Create test files

---
+++ lib/School/Library.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "Library/version"
+
+module School
+  module Library
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
